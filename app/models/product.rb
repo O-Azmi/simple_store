af3ed74637489bf8a842d863d54e9b1b14f52a94
Oazmi <azmioualid@gmail.com>
2024-02-14 13:11:50 -0600

    Faker added, and Saved validation (because I didn't in the last commit)

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
 
     #Validation to make sure that all these fields are filled with data when adding a product.
     validates :title, :price, :stock_quantity, presence: true
-    
+
 end
