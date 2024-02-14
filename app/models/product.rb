ec31eaf16d28e9c043794a7d968f1fa0a03bf632
Oazmi <azmioualid@gmail.com>
2024-02-14 13:09:20 -0600

    Validation added and tested

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+
+    #Validation to make sure that all these fields are filled with data when adding a product.
+    validates :title, :price, :stock_quantity, presence: true
+    
 end
